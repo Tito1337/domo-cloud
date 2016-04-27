e963b8e0aed698b85f75a9390f6c8b2215b38384
Alexis Leroy <alexis.leroy1991@gmail.com>
2016-04-27 09:34:57 +0200

    Log out..

--- server/views/dashboard.php
+++ server/views/dashboard.php
@@ -16,9 +16,7 @@
         <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav navbar-right">
                 <li><a href="#">Dashboard</a></li>
-                <li><a href="#">Settings</a></li>
-                <li><a href="#">Profile</a></li>
-                <li><a href="#">Help</a></li>
+                <li><a href="#">Log out</a></li>
             </ul>
             <form class="navbar-form navbar-right">
                 <input type="text" class="form-control" placeholder="Search...">
