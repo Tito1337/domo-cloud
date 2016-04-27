ccbb888476d8a8471e3e25b6cae801f28e930558
Tito1337 <tito@webtito.be>
2016-04-27 10:03:00 +0200

    Corrections mise en page

--- server/views/dashboard.php
+++ server/views/dashboard.php
@@ -1,55 +1,5 @@
 <?php include('header.php'); ?>
 <!-- TODO: test if ok in the dashboard -->
-<div id="wrapper" class="dashboard"> 
-
-<nav class="navbar navbar-inverse navbar-fixed-top">
-    <div class="container-fluid">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#">Domo-Cloud</a>
-        </div>
-        <div id="navbar" class="navbar-collapse collapse">
-            <ul class="nav navbar-nav navbar-right">
-                <li><a href="#">Dashboard</a></li>
-                <li><a href="#">Settings</a></li>
-                <li><a href="#">Profile</a></li>
-                <li><a href="#">Help</a></li>
-            </ul>
-            <form class="navbar-form navbar-right">
-                <input type="text" class="form-control" placeholder="Search...">
-            </form>
-        </div>
-    </div>
-</nav>
-
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-sm-3 col-md-2 sidebar">
-            <ul class="nav nav-sidebar">
-                <li class="active"><a href="#">Overview <span class="sr-only">(current)</span></a></li>
-                <li><a href="#">Reports</a></li>
-                <li><a href="#">Analytics</a></li>
-                <li><a href="#">Export</a></li>
-            </ul>
-            <!-- <ul class="nav nav-sidebar">
-                <li><a href="">Nav item</a></li>
-                <li><a href="">Nav item again</a></li>
-                <li><a href="">One more nav</a></li>
-                <li><a href="">Another nav item</a></li>
-                <li><a href="">More navigation</a></li>
-            </ul>
-            <ul class="nav nav-sidebar">
-                <li><a href="">Nav item again</a></li>
-                <li><a href="">One more nav</a></li>
-                <li><a href="">Another nav item</a></li>
-            </ul> -->
-        </div>
-        <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
             <h1 class="page-header">Bienvenue dans votre Dashboard</h1>
             <p>Connecté en tant que <?php echo $user['name']; ?></p>
             <p>Pièces de votre maison :
@@ -125,19 +75,4 @@
                     </tbody>
                 </table>
             </div> -->
-        </div>
-    </div>
-</div>
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="../js/jquery.min.js"><\/script>')</script>
-    <script src="../js/bootstrap.min.js"></script>
-    <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
-    <script src="../js//holder.min.js"></script>
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="../js/ie10-viewport-bug-workaround.js"></script>
-
     <?php include('footer.php'); ?>
--- server/views/header.php
+++ server/views/header.php
@@ -6,40 +6,71 @@
 <html lang="en">
   <head>
     <head>
-        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-        <meta name="description" content="">
-        <meta name="author" content="">
-        <link rel="icon" href="../../favicon.ico">
-
-    	<meta http-equiv="Content-Type" content="text/html" charset="UTF-8" />
+        <meta http-equiv="Content-Type" content="text/html" charset="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        
-    	<title>Domo-Cloud</title>
+
+        <title>Domo-Cloud</title>
+
+        <link rel="icon" href="../../favicon.ico">
         <link href="/css/style.css" rel="stylesheet" type="text/css" />
         <link href="/css/fullcalendar.css" rel="stylesheet" type="text/css" />
         <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-        <link href="css/ie10-viewport-bug-workaround.css" rel="stylesheet">
-        <link href="css/dashboard.css" rel="stylesheet">
-        <link href="css/signin.css" rel="stylesheet">
+        <link href="/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+        <link href="/css/dashboard.css" rel="stylesheet">
+        <link href="/css/signin.css" rel="stylesheet">
         <!-- Bootstrap core CSS -->
-        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="/css/bootstrap.min.css" rel="stylesheet">
 
         <script type='text/javascript' src='../js/jquery.min.js'></script>
         <script type='text/javascript' src='../js/moment.min.js'></script>
         <script type='text/javascript' src='../js/fullcalendar.js'></script>
         <script type='text/javascript' src='../js/fullcalendar-lang.js'></script>
-        
-
-        <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
-        <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-        <script src="js/ie-emulation-modes-warning.js"></script>
-
-        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-        <!--[if lt IE 9]>
-          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-        <![endif]-->
+        <script src="/js/bootstrap.min.js"></script>
+        <script src="/js/ie10-viewport-bug-workaround.js"></script>
     </head>
 <body>
 
+<div id="wrapper" class="dashboard">
+
+<nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Domo-Cloud</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="/">Dashboard</a></li>
+                <li><a href="/?page=logout">Logout</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-sm-3 col-md-2 sidebar">
+            <ul class="nav nav-sidebar">
+                    <?php
+                    $stmt = $db->prepare("SELECT * FROM rooms WHERE client_id=:client_id");
+                    $stmt->bindValue(':client_id', $user['id'], PDO::PARAM_INT);
+                    $stmt->execute();
+                    $rooms = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                    foreach($rooms as $room) {
+                        if(IsSet($_GET['room_id']) && $_GET['room_id'] == $room['id']) {
+                            $active = 'class="active"';
+                        } else {
+                            $active = '';
+                        }
+                        echo '<li '.$active.'><a href="/?page=room&room_id='.$room['id'].'">'.$room['name'].'</a></li>';
+                    }
+                    ?>
+            </ul>
+        </div>
+        <div class="col-sm-9 col-md-10 main">
