dce828a604a40af5657888a62b53f60b6af4a57c
Alexis Leroy <alexis.leroy1991@gmail.com>
2016-04-27 09:29:14 +0200

    offset + display fixed

--- server/views/dashboard.php
+++ server/views/dashboard.php
@@ -36,20 +36,8 @@
                 <li><a href="#">Analytics</a></li>
                 <li><a href="#">Export</a></li>
             </ul>
-            <!-- <ul class="nav nav-sidebar">
-                <li><a href="">Nav item</a></li>
-                <li><a href="">Nav item again</a></li>
-                <li><a href="">One more nav</a></li>
-                <li><a href="">Another nav item</a></li>
-                <li><a href="">More navigation</a></li>
-            </ul>
-            <ul class="nav nav-sidebar">
-                <li><a href="">Nav item again</a></li>
-                <li><a href="">One more nav</a></li>
-                <li><a href="">Another nav item</a></li>
-            </ul> -->
         </div>
-        <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+        <div class="col-sm-9 col-md-10 main">
             <h1 class="page-header">Bienvenue dans votre Dashboard</h1>
             <p>Connecté en tant que <?php echo $user['name']; ?></p>
             <p>Pièces de votre maison :
