60a79524ca13f2bd632009107c7d378ec18fa07b
Christophe De Wolf <tito@webtito.be>
2016-05-03 21:54:15 +0200

    Security enhancements

--- server/rpi.json.php
+++ server/rpi.json.php
@@ -2,11 +2,17 @@
 header('Content-Type: application/json');
 include('functions.php');
 
+if(!IsSet($_GET['client']) || !is_numeric($_GET['client'])) {
+    die("Contact us! jobs@domocloud.com");
+}
+
 $stmt = $db->prepare("SELECT * FROM clients WHERE id=:id");
-$stmt->bindValue(':id', $_GET['client_id'], PDO::PARAM_INT);
+$stmt->bindValue(':id', $_GET['client'], PDO::PARAM_INT);
 $stmt->execute();
 if($stmt->rowCount() == 1) {
     $user = $stmt->fetch(PDO::FETCH_ASSOC);
+} else {
+    die("Contact us! jobs@domocloud.com");
 }
 
 echo "{\n";
@@ -15,7 +21,7 @@ $stmt->bindValue(':client_id', $user['id'], PDO::PARAM_INT);
 $stmt->execute();
 $rooms = $stmt->fetchAll(PDO::FETCH_ASSOC);
 foreach($rooms as $room) {
-    echo '"'.$room['id'].'": '.get_current_order($room['id']).",\n";
+    echo '    "'.$room['id'].'": '.get_current_order($room['id']).",\n";
 }
 echo "}\n";
 ?>
